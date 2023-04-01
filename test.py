382e7ba214972ec674f1d52325450fefad838877
Slavick <92212345+slavicksnegirev@users.noreply.github.com>
2023-04-01 16:42:12 +0300

    update ACO

--- test.py
+++ test.py
@@ -58,116 +58,98 @@ G.add_edges_from([
 A = nx.adjacency_matrix(G)
 d = np.array(A.todense())
 
-# given values for the problems
-
-# d = np.array([[0, 10, 12, 11, 14]
-#                  , [10, 0, 13, 15, 8]
-#                  , [12, 13, 0, 9, 14]
-#                  , [11, 15, 9, 0, 16]
-#                  , [14, 8, 14, 16, 0]])
-
-iteration = 100
+n_iterations = 100
 n_ants = 15
-n_citys = 15
+n_nodes = len(G.nodes)
 
-# intialization part
+e = .5  # коэффициент испраения
+alpha = 1  # коэффициент феромона
+beta = 2  # коэффициент видимости
 
-m = n_ants
-n = n_citys
-e = .5  # evaporation rate
-alpha = 1  # pheromone factor
-beta = 2  # visibility factor
-
-# calculating the visibility of the next city visibility(i,j)=1/d(i,j)
+# подсчет видимости следующего узла: visibility(i,j)=1/d(i,j)
 
 visibility = 1 / d
 visibility[visibility == inf] = 0
 
-# intializing pheromne present at the paths to the cities
+# инициирующий феромон, присутствующий на дорогах, ведущих в узлы
 
-pheromne = .1 * np.ones((m, n))
+pheromne = .1 * np.ones((n_ants, n_nodes))
 
-# intializing the rute of the ants with size rute(n_ants,n_citys+1)
-# note adding 1 because we want to come back to the source city
+# инициализация маршрута муравьев с размером rute(n_ants,n_citys)
 
-rute = np.ones((m, n + 1))
+rute = 0 * np.ones((n_ants, n_nodes))
 
-for ite in range(iteration):
+for iteration in range(n_iterations):
 
-    rute[:, 0] = 1  # initial starting and ending positon of every ants '1' i.e city '1'
+    rute[:, 0] = 1  # начальное положение каждого муравья '1', то есть узла '1'
 
-    for i in range(m):
+    for i in range(n_ants):
 
-        temp_visibility = np.array(visibility)  # creating a copy of visibility
+        temp_visibility = np.array(visibility)
 
-        for j in range(n - 1):
-            # print(rute)
+        for j in range(n_nodes - 1):
+            # combine_feature = np.zeros(n_nodes)
+            # cum_prob = np.zeros(n_nodes)
 
-            combine_feature = np.zeros(5)  # intializing combine_feature array to zero
-            cum_prob = np.zeros(5)  # intializing cummulative probability array to zeros
+            cur_loc = int(rute[i, j] - 1)
 
-            cur_loc = int(rute[i, j] - 1)  # current city of the ant
+            temp_visibility[:, cur_loc] = 0  # видимость текущего узла равна нулю
 
-            temp_visibility[:, cur_loc] = 0  # making visibility of the current city as zero
+            p_feature = np.power(pheromne[cur_loc, :], beta)  # вычисление коэффициента феромона
+            v_feature = np.power(temp_visibility[cur_loc, :], alpha)  # вычисление коэффициента видимости
 
-            p_feature = np.power(pheromne[cur_loc, :], beta)  # calculating pheromne feature
-            v_feature = np.power(temp_visibility[cur_loc, :], alpha)  # calculating visibility feature
+            p_feature = p_feature[:, np.newaxis]  # добавление оси для создания size[n_nodes,1]
+            v_feature = v_feature[:, np.newaxis]  # добавление оси для создания size[n_nodes,1]
 
-            p_feature = p_feature[:, np.newaxis]  # adding axis to make a size[5,1]
-            v_feature = v_feature[:, np.newaxis]  # adding axis to make a size[5,1]
+            combine_feature = np.multiply(p_feature, v_feature)  # вычисление функции объединения
 
-            combine_feature = np.multiply(p_feature, v_feature)  # calculating the combine feature
+            total = np.sum(combine_feature)  # сумма всех характеристик
 
-            total = np.sum(combine_feature)  # sum of all the feature
+            probs = combine_feature / total  # нахождение вероятности элемента probs(i) = comine_feature(i)/total
 
-            probs = combine_feature / total  # finding probability of element probs(i) = comine_feature(i)/total
+            cum_prob = np.cumsum(probs)  # вычисление совокупной суммы
 
-            cum_prob = np.cumsum(probs)  # calculating cummulative sum
-            # print(cum_prob)
-            r = np.random.random_sample()  # randon no in [0,1)
-            # print(r)
-            city = np.nonzero(cum_prob > r)[0][0] + 1  # finding the next city having probability higher then random(r)
-            # print(city)
+            r = np.random.random_sample()  # рандомно число в диапазоне [0.0, 1.0)
 
-            rute[i, j + 1] = city  # adding city to route
-            if city == n:
+            node = np.nonzero(cum_prob > r)[0][0] + 1  # поиск следующего узла с вероятностью выше random(r)
+
+            rute[i, j + 1] = node  # добавление узла в путь
+            if node == n_nodes:
                 break
 
-        left = list(set([i for i in range(1, n + 1)]) - set(rute[i, :-2]))[0]  # finding the last untraversed city to route
+        # left = list(set([i for i in range(1, n_nodes + 1)]) - set(rute[i, :-2]))[0] # поиск последнего неизведанного города для маршрута
 
-        rute[i, -2] = left  # adding untraversed city to route
+        # rute[i, -2] = left  # adding untraversed city to route
 
-    rute_opt = np.array(rute)  # intializing optimal route
+    rute_opt = np.array(rute)  # инициализация оптимального пути
 
-    dist_cost = np.zeros((m, 1))  # intializing total_distance_of_tour with zero
+    dist_cost = np.zeros((n_ants, 1))  # инициализация стоимости пути
 
-    for i in range(m):
-
+    for i in range(n_ants):
         s = 0
-        for j in range(n - 1):
-            s = s + d[int(rute_opt[i, j]) - 1, int(rute_opt[i, j + 1]) - 1]  # calcualting total tour distance
+        for j in range(n_nodes - 1):
+            s = s + d[int(rute_opt[i, j]) - 1, int(rute_opt[i, j + 1]) - 1]  # расчет общего расстояния
 
-        dist_cost[i] = s  # storing distance of tour for 'i'th ant at location 'i'
+        dist_cost[i] = s  # сохранение расстояния 'i'-го муравья в положении 'i'
 
-    dist_min_loc = np.argmin(dist_cost)  # finding location of minimum of dist_cost
-    dist_min_cost = dist_cost[dist_min_loc]  # finging min of dist_cost
+    dist_min_loc = np.argmin(dist_cost)  # нахождение местоположения минимума dist_cost
+    dist_min_cost = dist_cost[dist_min_loc]  # нахождение минимума dist_cost
 
-    best_route = rute[dist_min_loc, :]  # intializing current traversed as best route
-    pheromne = (1 - e) * pheromne  # evaporation of pheromne with (1-e)
+    best_route = rute[dist_min_loc, :]  # инициализация текущего пройденного маршрута как наилучшего маршрута
+    pheromne = (1 - e) * pheromne  # испарение феромона с помощью (1-e)
 
-    for i in range(m):
-        for j in range(n - 1):
+    for i in range(n_ants):
+        for j in range(n_nodes - 1):
             dt = 1 / dist_cost[i]
-            pheromne[int(rute_opt[i, j]) - 1, int(rute_opt[i, j + 1]) - 1] = pheromne[int(rute_opt[i, j]) - 1, int(
-                rute_opt[i, j + 1]) - 1] + dt
-            # updating the pheromne with delta_distance
-            # delta_distance will be more with min_dist i.e adding more weight to that route  peromne
+            pheromne[int(rute_opt[i, j]) - 1, int(rute_opt[i, j + 1]) - 1] = pheromne[int(rute_opt[i, j]) - 1, int(rute_opt[i, j + 1]) - 1] + dt
+            # обновление феромона с помощью delta_distance
+            # delta_distance будет больше с min_dist, т.е.
+            # добавит больше веса этому маршруту на единицу площади
 
-print('route of all the ants at the end :')
+print('Конечные маршруты всех муравьев:')
 print(rute_opt)
-print()
-print('best path :', best_route)
-print('cost of the best path', int(dist_min_cost[0]) + d[int(best_route[-2]) - 1, 0])
+print('Кратчайший путь:', best_route)
+print('Стоимость кратчайшего пути: ', int(dist_min_cost[0]) + d[int(best_route[-2]) - 1, 0])
 
 
 
